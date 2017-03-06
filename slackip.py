4c425af1be1d4a4d4b424db5edf27f24b768f396
Atanu Ghosh <atanu@acm.org>
2017-03-06 07:56:44 +0000

    Save the IP address in a file, if the IP address hasn't
    changed don't write to the slack channel.

--- slackip.py
+++ slackip.py
@@ -34,7 +34,7 @@ def get_my_ip():
     conn.request("GET", "/ip")
     response = conn.getresponse()
 
-    return response.status, response.reason, response.read()
+    return response.status, response.reason, response.read()[:-1]
 
 def write_to_slack_channel(host, url, hostname, ipaddress):
     """
@@ -53,13 +53,39 @@ curl -X POST --data-urlencode 'payload={"channel": "#pandabot", "username": "web
         print(response.reason, ret, file=sys.stderr)
         return False
 
+    print("Sucessfully wrote to slack channel")
+
     return True
+
+def save_ip_address_to_file(filename, ipaddress):
+    """
+    Save my IP address to a file
+    """
+
+    save = open(filename, 'w')
+    save.write(ipaddress)
+    save.close()
+
+def read_ip_address_from_file(filename):
+    """
+    Read my saved IP address from file
+    """
+
+    try:
+        with open(filename, "r") as saved:
+            for line in saved:
+                return True, line
+    except IOError:
+        pass
+
+    return False, None
 
 USAGE =\
 """\
 usage: %s
 \t -h --host
 \t -u --url
+\t -s --save
 """
 
 def main():
@@ -74,8 +100,8 @@ def main():
         print(USAGE % sys.argv[0], end='', file=sys.stderr)
 
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], "h:u:",
-                                ["host=", "url="])
+        opts, _ = getopt.getopt(sys.argv[1:], "h:u:s:",
+                                ["host=", "url=", "save="])
     except getopt.GetoptError:
         usage()
         sys.exit(1)
@@ -83,23 +109,40 @@ def main():
 
     url = None
     host = None
+    save = None
 
     for option, arg in opts:
         if option in ("-h", "--host"):
             host = arg
         if option in ("-u", "--url"):
             url = arg
-
-    if not url or not host:
-        print("The host and URL must be provided", file=sys.stderr)
-        return
+        if option in ("-s", "--save"):
+            save = arg
 
     status, reason, ipaddress = get_my_ip()
     if status != 200:
         print("Problem", reason, file=sys.stderr)
         return
 
-    print(ipaddress)
+    print("Global IP address", ipaddress)
+
+    if not url or not host:
+        print("The host and URL must be provided", file=sys.stderr)
+        return
+
+    if save:
+        status, saved_ipaddress = read_ip_address_from_file(save)
+        if not status:
+            print("No saved IP address in", save)
+            save_ip_address_to_file(save, ipaddress)
+        else:
+            if ipaddress != saved_ipaddress:
+                print("Saved IP address does not match current IP address",
+                      saved_ipaddress, save)
+                save_ip_address_to_file(save, ipaddress)
+            else:
+                print("IP address has not changed")
+                return
 
     write_to_slack_channel(host, url, os.uname()[1], ipaddress)
 
